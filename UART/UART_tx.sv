f60d8a1c39ffe87a5fd640d67633aa81349bbb00
Gregor812 <juicefruit89@gmail.com>
2016-04-14 20:24:52 +0300

    wat

--- UART/UART_tx.sv
+++ UART/UART_tx.sv
@@ -3,4 +3,7 @@ module UART_tx (
 	input 	logic 				a_reset_i,
 	input	logic				tx,
 	input 	logic	[ 7 : 0 ]	tx_data,
-	input	logic				tx_data_strobe,
+	input	logic				tx_data_load_strobe
+	);
+
+endmodule // UART_tx
---
+++ labs/lab1/b1/top.sv
@@ -0,0 +1,17 @@
+module top (
+	input		logic [ 2**N - 1 : 0 ]	line,
+	output	logic [ N - 1 : 0 ]		code
+	);
+	
+	
+endmodule
+
+module #(parameter WIDTH = 4) coder (
+	input		logic [ 2**WIDTH - 1 : 0 ]	line,
+	output	logic [ WIDTH - 1 : 0 ]		code
+	);
+	
+	assign code
+	
+	
+endmodule
