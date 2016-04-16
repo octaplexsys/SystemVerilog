396ca4f55953879dcb957adf092b598bee99c64a
Gregor812 <juicefruit89@gmail.com>
2016-04-16 06:04:16 +0300

    Безуспешные попытки получить параметризированный шифратор 2**N --> N

--- labs/lab1/b1/top.sv
+++ labs/lab1/b1/top.sv
@@ -1,17 +1,27 @@
+parameter N = 8;
+
 module top (
-	input		logic [ 2**N - 1 : 0 ]	line,
+	input	logic [ 2**N - 1 : 0 ]	line,
 	output	logic [ N - 1 : 0 ]		code
 	);
 	
+	coder #( N ) coder_instance ( .* );
 	
 endmodule
 
-module #(parameter WIDTH = 4) coder (
-	input		logic [ 2**WIDTH - 1 : 0 ]	line,
+module coder #(parameter WIDTH = 4) (
+	input	logic [ 2**WIDTH - 1 : 0 ]	line,
 	output	logic [ WIDTH - 1 : 0 ]		code
 	);
 	
-	assign code
+	genvar i;
 	
+	generate
+		for( i = 2**WIDTH - 1; i >= 0; ++i ) begin : forloop
+			if ( line[i] ) begin
+				assign code = i;
+			end
+		end
+	endgenerate
 	
 endmodule
