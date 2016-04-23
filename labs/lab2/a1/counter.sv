0b06534761d37a0557cb21f8937c226d5ba283e7
Gregor812 <juicefruit89@gmail.com>
2016-04-24 00:00:20 +0300

    watches

--- labs/lab2/a1/counter.sv
+++ labs/lab2/a1/counter.sv
@@ -1,4 +1,21 @@
-module counter #( parameter WIDTH = 4, parameter OVERFLOW = 60 ) (
-	input	logic			rst_i,
-	input	logic			clk_i,
-	input	logic			en_i,
+module counter #( parameter WIDTH = 5, parameter OVERFLOW = 2**WIDTH - 1 ) (
+	input	logic					rst_i,
+	input	logic					clk_i,
+	input	logic					en_i,
+	output	logic [ WIDTH - 1 : 0 ] value
+	);
+	
+	always_ff @( posedge clk_i or posedge rst_i ) begin
+		if ( rst_i ) begin
+			value <= '0;
+		end else begin
+			if ( en_i ) begin
+				if ( value < OVERFLOW - 1 ) begin
+					value <= value + 1'b1;
+				end else begin
+					value <= '0;
+				end
+			end
+		end
+	end
+endmodule
--- labs/lab2/a1/top.sv
+++ labs/lab2/a1/top.sv
@@ -1,12 +1,20 @@
 module watches (
-	input	logic			rst_i,
-	input	logic			clk_i,
-	input	logic			up,
-	input	logic			down,
-	output	logic [ 6 : 0 ]	msec,
-	output	logic [ 5 : 0 ]	sec,
-	output	logic [ 5 : 0 ]	min,
-	output	logic [ 4 : 0 ]	min
+	input	logic					rst_i,
+	input	logic					clk_i,
+	input	logic					up,
+	input	logic					down,
+	output	logic [ 6 : 0 ]			msec,
+	output	logic [ 5 : 0 ]			sec,
+	output	logic [ 5 : 0 ]			min,
+	output	logic [ 4 : 0 ]			hour
 	);
 	
+	localparam INPUT_FREQ	50_000_000;
+	localparam MS_CYCLE		50_000;
+	localparam MS_WIDTH		$bits( MS_CYCLE );
 	
+	logic [ MS_WIDTH - 1 : 0 ] input_freq_div;
+	
+	counter #( .WIDTH( MS_WIDTH ), .OVERFLOW( MS_CYCLE ) ) ms_counter( .*, .en_i( 1'b1 ), input_freq_div);
+	
+endmodule
