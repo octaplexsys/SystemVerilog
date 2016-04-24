1236ad536f7c737a82d33ded2eb63159daf2d87f
Gregor812 <juicefruit89@gmail.com>
2016-04-25 00:00:37 +0300

    Оптимизация схемы

--- labs/lab2/a1/top.sv
+++ labs/lab2/a1/top.sv
@@ -15,6 +15,16 @@ module top (
 	
 	logic [ MS_WIDTH - 1 : 0 ] input_freq_div;
 	
+	logic clock_cycle;
+	logic msec_ovf;
+	logic sec_ovf;
+	logic min_ovf;
+	
+	assign clock_cycle	= ( input_freq_div	== 0 );
+	assign msec_ovf		= ( msec			== 10'd999	);
+	assign sec_ovf		= ( sec				== 6'd59	);
+	assign min_ovf		= ( min				== 6'd59	);
+	
 	counter
 	#( .WIDTH( MS_WIDTH ),		.OVERFLOW( MS_CYCLE ) )	msec_div		(
 		.*,
@@ -25,28 +35,28 @@ module top (
 	counter
 	#( .WIDTH( $bits( msec ) ),	.OVERFLOW( 1000 ) )		msec_counter	(
 		.*,
-		.en_i( !input_freq_div ),
+		.en_i( clock_cycle ),
 		.value( msec )
 	);
 	
 	counter
 	#( .WIDTH( $bits( sec ) ),	.OVERFLOW( 60 ) ) 		sec_counter		(
 		.*,
-		.en_i( !input_freq_div && ( msec == 10'd999 ) ),
+		.en_i( clock_cycle && msec_ovf ),
 		.value( sec )
 	);
 	
 	counter
 	#( .WIDTH( $bits( min ) ),	.OVERFLOW( 60 ) ) 		min_counter		(
 		.*,
-		.en_i( !input_freq_div && ( sec == 6'd59 ) && ( msec == 10'd999 ) ),
+		.en_i( clock_cycle && msec_ovf && sec_ovf ),
 		.value( min )
 	);
 	
 	counter
 	#( .WIDTH( $bits( hour ) ),	.OVERFLOW( 24 ) )	 	hour_counter	(
 		.*,
-		.en_i( !input_freq_div && ( min == 6'd59 ) && ( sec == 6'd59 ) && ( msec == 10'd999 ) ),
+		.en_i( clock_cycle && msec_ovf && sec_ovf && min_ovf ),
 		.value( hour )
 	);
 	
