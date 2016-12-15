64bcefe08a11f52d0a083a17ebf1311b2e24d0ed
Gregor812 <juicefruit89@gmail.com>
2016-12-15 16:24:57 +0300

    Удаление неиспользуемых файлов

--- SPI/SPI_slave/top.sdc
+++
@@ -1,2 +0,0 @@
-derive_clock_uncertainty
-create_clock -name main_clock -period 20.000 [get_ports {clk_i}]
--- UART/UART_tx.sv
+++
@@ -1,9 +0,0 @@
-module UART_tx (
-	input 	logic 				clk_i,
-	input 	logic 				a_reset_i,
-	input	logic				tx,
-	input 	logic	[ 7 : 0 ]	tx_data,
-	input	logic				tx_data_load_strobe
-	);
-
-endmodule // UART_tx
